e15515ebd4b464a8e2f187464acf5996424b2dc2
Eben Varghese Paul <eben92paul@gmail.com>
2023-06-19 22:46:56 +0530

    Initail Commit

---
+++ RabbitReceiver/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConnectionFactory factory = new ConnectionFactory
+        {
+            Uri = new Uri("amqp://guest:guest@localhost:5672"),
+            ClientProvidedName = "Rabbit Receiver1 App"
+        };
+
+        IConnection cnn = factory.CreateConnection();
+        IModel channel = cnn.CreateModel();
+        string exchangeName = "DemoExchange";
+
+        string routingKey = "demo-routing-key";
+        string queueName = "DemoQueue";
+
+        channel.ExchangeDeclare(exchangeName, ExchangeType.Direct, durable: false, autoDelete: false, arguments: null);
+        channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+        channel.QueueBind(queueName, exchangeName, routingKey, null);
+        channel.BasicQos(0,1,false);
+        
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (sender ,args) =>
+        {
+            Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+            var body =args.Body.ToArray();
+            string message=Encoding.UTF8.GetString(body); 
+            Console.WriteLine($"Message Recieved: {message}");
+            channel.BasicAck(args.DeliveryTag,false);
+        };
+        string consumerTag=channel.BasicConsume(queueName,false,consumer);
+        Console.ReadLine();
+        channel.BasicCancel(consumerTag);
+        channel.Close();
+        cnn.Close();
+    }
+}
